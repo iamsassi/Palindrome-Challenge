c5b5ce33789ec00c189b24fef92ca5de59b3dda0
iamsassi <marcus.sassi@gmail.com>
2021-06-07 17:33:10 +0200

    Program ready to use

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Palindrome_Challenge
 {
@@ -6,7 +8,44 @@ namespace Palindrome_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The Palindrome Challenge
+
+            Console.WriteLine("Let's begin:");
+
+            bool loop = true;
+            string checkword ="";
+
+            while(loop)
+            {
+                var userInput = Console.ReadLine();
+                int counter = userInput.Length -1;
+
+                while(counter != -1)
+                {
+                    checkword += string.Concat(userInput[counter]);
+                    counter--;
+                }
+
+                if (checkword == userInput)
+                {
+                    Console.WriteLine($"The word {checkword} is palindrome and length is {checkword.Length}");
+                    checkword = "";
+                }
+                else if(userInput == "exit")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine($"The word {checkword} is not a palindrome");
+                    checkword = "";
+                }
+
+            }
+
+            Console.ReadLine();
+            
+
         }
     }
 }
